830c6dadccc14e9136928a67e42857552c08f2f3
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-02-21 15:21:26 -0500

    Added UT Project

---
+++ SSUT/Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SSUT
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LoadTitleCellTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
