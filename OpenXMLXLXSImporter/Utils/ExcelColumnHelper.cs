436a71b2d6e6e4acbb15336ca1cfc1b101ca944e
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-03-06 17:35:24 -0500

    column helper

---
+++ OpenXMLXLXSImporter/Utils/ExcelColumnHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenXMLXLSXImporter.Utils
+{
+    public static class ExcelColumnHelper
+    {
+        public const uint BASE = 26U;
+        public static uint GetColumnStringAsIndex(string s)
+        {
+            char[] chars = s.ToArray();
+            uint[] charValuesAtZero = new uint[chars.Length];
+            for (uint i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] < 128 && Char.IsLetter(chars[i]))
+                {
+                    charValuesAtZero[i] = (uint)(Char.ToUpperInvariant(chars[i]) - 65);
+                }
+                else
+                {
+                    throw new ArgumentException("unexpected character exception");
+                }
+            }
+            
+            uint sum = 0;
+
+            for (uint i = 0; i < charValuesAtZero.Length;i++)
+            {
+                sum += (uint)(charValuesAtZero[i] * Math.Floor(Math.Pow(BASE, i)));
+            }
+
+            uint log = (uint)(charValuesAtZero.Length - 1);
+            sum = sum + Normalize(log);
+
+            return sum;
+        }
+
+        public static uint Normalize(uint log)
+            => (uint)(Math.Pow(BASE, log) - 1) * BASE / (BASE - 1U);
+
+        public static string GetColumnIndexAsString(uint index)
+        {
+            char[] results;
+            //hmm looks like were going to need to use... Math!!!
+
+            //hmm looks like excel is not base 26 as I initally though looks like we are going to have to do extra maths
+            //and by do maths I mean copy pasta
+
+            //Grabs the log26 of index + some additional processing for the Edges ie AZ (copy pasta magic)
+            uint log = (uint)Math.Floor(Math.Log(((index * (BASE - 1U)) / BASE) + 1U, BASE));
+            uint digits = log + 1U;
+            results = new char[digits];
+
+            uint normalizedIndex = index - Normalize(log);
+
+            for (uint count = 0; normalizedIndex + digits > 0; digits--)
+            {
+                uint digit = normalizedIndex % BASE;
+                char c = (char)(digit + 65);
+                results[count++] = c;
+                
+                normalizedIndex = normalizedIndex / BASE;
+            }
+            return new string(results);
+            }
+    }
+}
---
+++ SSUT/EncodingTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using OpenXMLXLSXImporter.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSUT
+{
+    public class EncodingTest
+    {
+        private void TestRange(string[] testColumns)
+        {
+            uint[] indexs = new uint[testColumns.Length];
+            for (uint i = 0; i < testColumns.Length; i++)
+            {
+                indexs[i] = ExcelColumnHelper.GetColumnStringAsIndex(testColumns[i]);
+            }
+
+            //Reconvert it back
+            string[] unencoded = new string[indexs.Length];
+
+            for (uint i = 0; i < indexs.Length; i++)
+            {
+                unencoded[i] = ExcelColumnHelper.GetColumnIndexAsString(indexs[i]);
+            }
+
+            //Ensure they are equal to the original value
+            for (uint i = 0; i < unencoded.Length; i++)
+            {
+                Assert.IsTrue(testColumns[i] == unencoded[i]);
+            }
+        }
+
+        [Test]
+        public void IndexEncodingTest2Digits()
+        {
+            string[] testColumns = new string[] { "X", "Y", "Z", "AA", "AB", "AC" };
+            TestRange(testColumns);
+        }
+        [Test]
+        public void MoveSecondSymbolTest()
+        {
+            string[] testColumns = new string[] {  "AZ", "BA", "BB", "BC" };
+            TestRange(testColumns);
+        }
+
+        [Test]
+        public void IndexEncodingTest3Digits()
+        {
+            string[] testColumns = new string[] { "ZX", "ZY", "ZZ", "AAA", "AAB", "AAC" };
+            TestRange(testColumns);
+        }
+        [Test]
+        public void TestSingleIndex()
+        {
+            //const uint V = 283;
+            const uint V = 2233;
+            string v = ExcelColumnHelper.GetColumnIndexAsString(V);
+            uint final = ExcelColumnHelper.GetColumnStringAsIndex(v);
+        }
+
+
+        [Test]
+        public void IndexesToChars()
+        {
+            const uint START = 0;
+            const uint MAX_END = 16384 * 20;
+            const uint TEST_CAP = MAX_END - START;
+
+            string[] testColumns = new string[TEST_CAP];
+            for(uint i = START; i < TEST_CAP;i++)
+            {
+                testColumns[i - START] = ExcelColumnHelper.GetColumnIndexAsString(i);
+            }
+
+            for(uint i = START;i < TEST_CAP;i++)
+            {
+                string column = testColumns[i - START];
+                uint result = ExcelColumnHelper.GetColumnStringAsIndex(column);
+                Assert.IsTrue(result == i);
+            }
+        }
+    }
+}
