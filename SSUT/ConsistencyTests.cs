41bcf48a44895ab87f679acf082d38db1155156b
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-04-09 19:01:26 -0400

    this makes it easier to debug freezing

--- SSUT/ConsistencyTests.cs
+++ SSUT/ConsistencyTests.cs
@@ -15,11 +15,46 @@ namespace SSUT
     public  class ConsistencyTests : BaseTest
     {
         public const int LOOPS = 100000;
+        public const int THRADS = 8;
+
+
+        public static void TestConsistency(Action a)
+        {
+            //this method is alot easier to track when things freeze the the ParallerFor
+            Task[] pool = new Task[THRADS];
+            int poolOffset = 0;
+            for (int i = 0; i < LOOPS; i++)
+            {
+                Task t = Task.Run(a);
+                if (i < pool.Length)
+                {
+                    pool[i] = t;
+                }
+                else
+                {
+                    if (poolOffset >= pool.Length)
+                    {
+                        poolOffset = 0;
+                    }
+                    pool[poolOffset].Wait();
+                    pool[poolOffset] = t;
+                    poolOffset++;
+                }
+            }
+            for(int i =0;i < pool.Length;i++)
+            {
+                if(pool[i] != null)
+                {
+                    pool[i].Wait();
+                }
+            }
+        }
+
 
         private void LoopUsingSameDataSet(BaseTest context,Action r)
         {
             context.importer = this.importer;
-            Parallel.For(0, LOOPS, new ParallelOptions { }, (i) =>
+            TestConsistency(() =>
             {
                 r();
             });
@@ -37,7 +72,7 @@ namespace SSUT
                 }
                 data = baseStream.ToArray();
             }
-            Parallel.For(0, LOOPS, new ParallelOptions { MaxDegreeOfParallelism = 2 }, (i) =>
+            TestConsistency(() =>
             {
                 using (MemoryStream ms = new MemoryStream(data, false))
                 {
@@ -46,11 +81,9 @@ namespace SSUT
                         TProp context = new TProp();
                         context.importer = importer;
                         r(context);
-
                     }
                 }
             });
-
         }
 
 
